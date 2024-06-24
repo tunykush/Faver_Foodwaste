0d48762b5f28a9127ec86c4f1b01dcbc271c5d41
tunykush <kyoukkuitsuki@gmail.com>
2024-06-25 01:42:59 +1000

    fix folder name

--- milestone3-thymeleaf/src/main/resources/js/progressbar.js
+++
@@ -1,9 +0,0 @@
-const content = document.getElementById('content')
-        const progressBar = document.getElementById('progress-bar')
-
-        document.addEventListener('scroll', () => {
-            const scrollTop = window.scrollY || document.documentElement.scrollTop
-            const scrollHeight = content.scrollHeight - window.innerHeight
-            const percentage = (scrollTop / scrollHeight) * 100
-            progressBar.style.width = percentage + '%'
-        });
